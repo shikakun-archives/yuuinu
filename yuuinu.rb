5c9aacec687d143fac8de91fbca2d648f199234c
shikakun <k@u-n.jp>
2013-08-12 20:46:12 +0900

    チェックインした時間を現在からの相対時間を表示するようにした

--- yuuinu.rb
+++ yuuinu.rb
@@ -61,6 +61,23 @@ def tweet(tweets)
   end
 end
 
+def timeago(times)
+  diff = Time.now.to_i - times.to_i
+  case diff
+    when 0 then 'just now'
+    when 1 then 'a second ago'
+    when 2..59 then diff.to_s+' seconds ago' 
+    when 60..119 then 'a minute ago' #120 = 2 minutes
+    when 120..3540 then (diff/60).to_i.to_s+' minutes ago'
+    when 3541..7100 then 'an hour ago' # 3600 = 1 hour
+    when 7101..82800 then ((diff+99)/3600).to_i.to_s+' hours ago' 
+    when 82801..172000 then 'a day ago' # 86400 = 1 day
+    when 172001..518400 then ((diff+800)/(60*60*24)).to_i.to_s+' days ago'
+    when 518400..1036800 then 'a week ago'
+    else ((diff+180000)/(60*60*24*7)).to_i.to_s+' weeks ago'
+  end
+end
+
 not_found do
   redirect "/"
 end
@@ -87,7 +104,9 @@ get "/auth/:provider/callback" do
     :nickname => session['nickname'],
     :image => session['image'],
     :token => session['token'],
-    :secret => session['secret']
+    :secret => session['secret'],
+    :created_at => Time.now,
+    :updated_at => Time.now
   )
   redirect "/"
 end
