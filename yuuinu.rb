e00e753d2a545bb8f73641d38b6af0ff5ae11d56
shikakun <k@u-n.jp>
2013-08-13 00:16:43 +0900

    時間の相対表示の文言を変更した, 重複した記述を省略した

--- yuuinu.rb
+++ yuuinu.rb
@@ -66,17 +66,14 @@ end
 def timeago(times)
   diff = Time.now.to_i - times.to_i
   case diff
-    when 0 then '今'
-    when 1 then '1秒前'
-    when 2..59 then diff.to_s+'秒前' 
-    when 60..119 then '1分前' #120 = 2 minutes
-    when 120..3540 then (diff/60).to_i.to_s+'分前'
-    when 3541..7100 then '1時間前' # 3600 = 1 hour
-    when 7101..82800 then ((diff+99)/3600).to_i.to_s+'時間前' 
-    when 82801..172000 then '昨日' # 86400 = 1 day
-    when 172001..518400 then ((diff+800)/(60*60*24)).to_i.to_s+'日前'
+    when 0 then 'たったいま'
+    when 1..59 then diff.to_s+'秒前に' 
+    when 60..3540 then (diff/60).to_i.to_s+'分前に'
+    when 3541..82800 then ((diff+99)/3600).to_i.to_s+'時間前に'
+    when 82801..172000 then 'きのう'
+    when 172001..518400 then ((diff+800)/(60*60*24)).to_i.to_s+'日前に'
     when 518400..1036800 then '先週'
-    else ((diff+180000)/(60*60*24*7)).to_i.to_s+'週間前'
+    else ((diff+180000)/(60*60*24*7)).to_i.to_s+'週間前に'
   end
 end
 
