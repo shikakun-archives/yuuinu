cbd65b38606cee3192e2fef8e9984765f051b18f
shikakun <k@u-n.jp>
2013-08-12 20:50:36 +0900

    相対時間を日本語化した

--- yuuinu.rb
+++ yuuinu.rb
@@ -64,17 +64,17 @@ end
 def timeago(times)
   diff = Time.now.to_i - times.to_i
   case diff
-    when 0 then 'just now'
-    when 1 then 'a second ago'
-    when 2..59 then diff.to_s+' seconds ago' 
-    when 60..119 then 'a minute ago' #120 = 2 minutes
-    when 120..3540 then (diff/60).to_i.to_s+' minutes ago'
-    when 3541..7100 then 'an hour ago' # 3600 = 1 hour
-    when 7101..82800 then ((diff+99)/3600).to_i.to_s+' hours ago' 
-    when 82801..172000 then 'a day ago' # 86400 = 1 day
-    when 172001..518400 then ((diff+800)/(60*60*24)).to_i.to_s+' days ago'
-    when 518400..1036800 then 'a week ago'
-    else ((diff+180000)/(60*60*24*7)).to_i.to_s+' weeks ago'
+    when 0 then '今'
+    when 1 then '1秒前'
+    when 2..59 then diff.to_s+'秒前' 
+    when 60..119 then '1分前' #120 = 2 minutes
+    when 120..3540 then (diff/60).to_i.to_s+'分前'
+    when 3541..7100 then '1時間前' # 3600 = 1 hour
+    when 7101..82800 then ((diff+99)/3600).to_i.to_s+'時間前' 
+    when 82801..172000 then '昨日' # 86400 = 1 day
+    when 172001..518400 then ((diff+800)/(60*60*24)).to_i.to_s+'日前'
+    when 518400..1036800 then '先週'
+    else ((diff+180000)/(60*60*24*7)).to_i.to_s+'週間前'
   end
 end
 
