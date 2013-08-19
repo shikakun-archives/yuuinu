818997325e1a286876492671553ef65ede1ae7c6
shikakun <k@u-n.jp>
2013-08-20 00:18:47 +0900

    タブで複数の YouTube 動画から選択できるようにした

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,21 @@
 $(function() {
   video_size_setting();
 
+  var default_video = 'v2pre';
+  var youtube_id = {
+    'v1': 'DomXocSJ-ZE',
+    'v2pre': 'y0IgRZn9QT8'
+  };
+  $('#youtube').attr('src', '//www.youtube.com/embed/' + youtube_id[default_video] + '?autoplay=1&vq=hd1080');
+  $('#' + default_video).addClass('active');
+
+  $('.tab').click(function() {
+    $('.tab').removeClass('active');
+    $(this).addClass('active');
+    var select_id = $(this).attr('id');
+    $('#youtube').attr('src', '//www.youtube.com/embed/' + youtube_id[select_id] + '?autoplay=1&vq=hd1080');
+  });
+
   $(window).resize(function() {
     video_size_setting();
   });
